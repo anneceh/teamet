64dcef4bfe31c3a2804f6910217c9aa25a345544
Peder Lång Skeidsvoll <pls@computas.com>
2016-11-14 20:15:07 +0100

    s

--- index.js
+++ index.js
@@ -86,40 +86,26 @@ socket.on('connect', function () {
       log('SensorTag logger: connected to IOT-Hub at ' + HUB_ADDRESS);
 });
 
-
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+router.get('/api', function(req, res) {
     res.json({ temp: temp, magnet: magnet, accelometer: accelometer });   
 });
 
-// more routes for our API will happen here
+router.get('/', function(req, res) {
+    res.sendFile('index.html') 
+});
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
