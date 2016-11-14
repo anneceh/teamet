3e3d4e3f14538e8dc0e7681e85d2b61136ed9c9e
Peder Lång Skeidsvoll <pls@computas.com>
2016-11-14 19:00:21 +0100

    xxdew

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 var fs = require('fs');
-var socket = require('socket.io-client')(HUB_ADDRESS);
-var SensorTag = require('sensortag');
-var express = require('express')
 
 var HUB_ADDRESS = process.argv[2] || 'http://localhost:3000';
 var ID = process.argv[3] || 'sensortag1';
 var SENSORTAG_ADDRESS = process.argv[4] || 'b0:b4:48:c9:57:81';
 
-var temp = 0;
 
-var log = function(text) {
-  if(text) {
-    console.log(text);
-  }
-}
+var socket = require('socket.io-client')(HUB_ADDRESS);
+var SensorTag = require('sensortag');
 
 var payload = function(sensor, data) {
   return {
@@ -29,9 +22,9 @@ var connected = new Promise((resolve, reject) => SensorTag.discoverByAddress(SEN
 
 var sensor = connected.then(function(tag) {
   log('SensorTag: connected to ' + SENSORTAG_ADDRESS);
-  tag.enableIrTemperature(log);
-  tag.notifyIrTemperature(log);
-  tag.setIrTemperaturePeriod(1000, log);
+  tag.enableIrTemperature();
+  tag.notifyIrTemperature();
+  tag.setIrTemperaturePeriod(1000);
 
   return tag;
 });
@@ -39,20 +32,10 @@ var sensor = connected.then(function(tag) {
 sensor.then(function(tag) {
   tag.on('irTemperatureChange', function(objectTemp, ambientTemp) {
     socket.emit('sensor:data', payload('irTemperature', {objectTemp, ambientTemp}));
-    temp = ambientTemp;
+    console.log(ambientTemp);
   });
 });
 
 socket.on('connect', function () {
-      log('SensorTag logger: connected to IOT-Hub at ' + HUB_ADDRESS);
+      console.log('SensorTag logger: connected to IOT-Hub at ' + HUB_ADDRESS);
 });
-
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send(temp)
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
