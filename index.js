c51f78b9f428ba1b5941b604b6653cd8a27f64bd
Peder Lång Skeidsvoll <pls@computas.com>
2016-11-14 19:14:15 +0100

    s

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var SENSORTAG_ADDRESS = process.argv[4] || 'b0:b4:48:c9:57:81';
 var express = require('express');
 var socket = require('socket.io-client')(HUB_ADDRESS);
 var SensorTag = require('sensortag');
+var temp = 0;
 
 var log = function(text) {
   if(text) {
@@ -50,6 +51,7 @@ sensor.then(function(tag) {
     socket.emit('sensor:data', payload('irTemperature', {objectTemp, ambientTemp}));
     log(ambientTemp);
     writeToFile('temp.txt', ambientTemp);
+    temp = ambientTemp;
   });
 });
 
